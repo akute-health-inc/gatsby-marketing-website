ec8c7886d050319d4f1be8a78e2b95e7e8f78c28
Sharud Agarwal <agarwal.sharud@gmail.com>
2019-07-26 13:36:58 -0500

    add more info to faq about tiered pricing

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -1,87 +1,129 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Navigation from '../components/navigation';
-import SEO from '../components/seo';
-import Footer from '../components/Footer';
-import PricingCard from '../components/PricingCard';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react'
+import { graphql } from 'gatsby'
+import Navigation from '../components/navigation'
+import SEO from '../components/seo'
+import Footer from '../components/Footer'
+import PricingCard from '../components/PricingCard'
+import 'bootstrap/dist/css/bootstrap.css'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
-import {
-  Container,
-  CardGroup,
-} from 'reactstrap';
+import { Container, CardGroup } from 'reactstrap'
 
-const PricingPage = ({data}) => (
+const PricingPage = ({ data }) => (
   <div>
     <Navigation data={data.allContentfulNavbar} />
     <SEO title="Pricing" />
-    <Container style={{minHeight: "80vh"}}>
+    <Container style={{ minHeight: '80vh' }}>
       <div className="text-center">
         <h1 className="display-4">Pricing</h1>
-        <p className="lead">{data.allContentfulPricingPage.edges[0].node.subtitle.subtitle}</p>
+        <p className="lead">
+          {data.allContentfulPricingPage.edges[0].node.subtitle.subtitle}
+        </p>
       </div>
       <CardGroup>
-        <PricingCard data={data.allContentfulPricingPage.edges[0].node} styles={data.allContentfulStyles.edges[0].node.childContentfulStylesButtonJsonNode} signupPage={true}/>
+        <PricingCard
+          data={data.allContentfulPricingPage.edges[0].node}
+          styles={
+            data.allContentfulStyles.edges[0].node
+              .childContentfulStylesButtonJsonNode
+          }
+          signupPage={true}
+        />
       </CardGroup>
     </Container>
     <Container>
-      <br />
       <div className="row">
         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4" />
-          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <h3 style={{justifyContent: "center", display: "flex"}}>FAQ</h3>
-          </div>
+        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <h3 style={{ justifyContent: 'center', display: 'flex' }}>FAQ</h3>
+        </div>
         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4" />
       </div>
       <br />
       <div className="row">
-        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <h5 style={{display: "flex", justifyContent: "center"}}>New solo practice? Need help to grow?</h5>
-          <p>We want to help new direct care providers gain patients and grow. We have a startup program for practices that have fewer than 100 patients and need help growing. If this sounds like you, then email us at{' '}
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <h5 style={{ display: 'flex', justifyContent: 'center' }}>
+            New solo practice? Need help to grow?
+          </h5>
+          <p>
+            We want to help new direct care providers gain patients and grow. We
+            have a startup program for practices that have fewer than 100
+            patients and need help growing. If this sounds like you, then email
+            us at{' '}
             <OutboundLink
-              href={`mailto:${data.allContentfulFooter.edges[0].node.companyContactEmail}`}
+              href={`mailto:${
+                data.allContentfulFooter.edges[0].node.companyContactEmail
+              }`}
               target="_blank"
               rel="noopener noreferrer"
             >
               {data.allContentfulFooter.edges[0].node.companyContactEmail}
-            </OutboundLink>
-            {' '}with the subject, <b>New Practice</b>, and we will discuss the details.</p>
+            </OutboundLink>{' '}
+            with the subject, <b>New Practice</b>, and we will discuss the
+            details.
+          </p>
+        </div>
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <h5>How does tiered pricing work?</h5>
+          <p>
+            Your first 100 patients will cost $1/ea/mo. Second 100: $0.75/ea.
+            Third 100: $0.50/ea. After that: $0.25/ea. We do this so that your
+            startup costs are low and your costs at scale are reasonable.
+          </p>
         </div>
       </div>
       <div className="row">
         <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
           <h5>What is considered an active patient?</h5>
-          <p>Any unique patient who you have billed in the past month.</p>
+          <p>
+            Any unique patient who you have billed in the past month. We get
+            this information from your practice management system, usually Hint.
+          </p>
         </div>
         <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
           <h5>Why price by active patient?</h5>
-          <p>We want to support you as your practice grows. By charging per active patient, we can offer a low cost solution while you are in the early stages while also being incentivized to help you grow. Alignment of incentives are good in all businesses, not just healthcare :)</p>
+          <p>
+            We want to support you as your practice grows. By charging per
+            active patient, we can offer a low cost solution while you are in
+            the early stages while also being incentivized to help you grow.
+            Alignment of incentives are good in all businesses, not just
+            healthcare :)
+          </p>
         </div>
       </div>
       <div className="row">
         <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
           <h5>Can I import records from my previous EHR to Akute Health?</h5>
-          <p>Yes, just contact us at{' '}
+          <p>
+            Yes, just contact us at{' '}
             <OutboundLink
-              href={`mailto:${data.allContentfulFooter.edges[0].node.companyContactEmail}`}
+              href={`mailto:${
+                data.allContentfulFooter.edges[0].node.companyContactEmail
+              }`}
               target="_blank"
               rel="noopener noreferrer"
             >
               {data.allContentfulFooter.edges[0].node.companyContactEmail}
-            </OutboundLink>
-            {' '}and we will help you from there.</p>
+            </OutboundLink>{' '}
+            and we will help you from there.
+          </p>
         </div>
         <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-          <h5>What if I just want to use the scheduling and/or communication?</h5>
-          <p>You can do that! Just email us at{' '}
+          <h5>
+            What if I just want to use the scheduling and/or communication?
+          </h5>
+          <p>
+            You can do that! Just email us at{' '}
             <OutboundLink
-              href={`mailto:${data.allContentfulFooter.edges[0].node.companyContactEmail}`}
+              href={`mailto:${
+                data.allContentfulFooter.edges[0].node.companyContactEmail
+              }`}
               target="_blank"
               rel="noopener noreferrer"
             >
               {data.allContentfulFooter.edges[0].node.companyContactEmail}
-            </OutboundLink>
-            {' '}for separate pricing and onboarding details.</p>
+            </OutboundLink>{' '}
+            for separate pricing and onboarding details.
+          </p>
         </div>
       </div>
     </Container>
@@ -90,7 +132,7 @@ const PricingPage = ({data}) => (
   </div>
 )
 
-export default PricingPage;
+export default PricingPage
 
 export const pageQuery = graphql`
   query {
@@ -137,7 +179,7 @@ export const pageQuery = graphql`
     allContentfulFooter {
       edges {
         node {
-          companyName 
+          companyName
           companyContactEmail
           driftAppId
           socialPages
