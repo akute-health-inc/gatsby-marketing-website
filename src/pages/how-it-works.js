0bb340aef40537722eaf8f5263d2d6560f22669c
Sharud Agarwal <agarwal.sharud@gmail.com>
2019-09-30 12:50:13 -0500

    fix new link for document video

--- src/pages/how-it-works.js
+++ src/pages/how-it-works.js
@@ -57,7 +57,7 @@ const HowItWorksPage = ({ data, location }) => (
             width="100%"
             // height="100%"
             height="315"
-            src="https://www.youtube.com/embed/lCBQygskvRo"
+            src="https://www.youtube.com/embed/j8xvi8joGxk"
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
